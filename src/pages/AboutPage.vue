b054f30e7a2081c6f9207e4a293ec5b816fcb981
Zed <wolknot@mail.ru>
2025-01-21 09:24:07 +0300

    about0

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,88 +1,78 @@
 <template>
   <div class="text-center q-pa-md flex flex-md-center">
-
-    <q-card class="my-card q-pa-md q-ma-md"  >
+    <q-card v-for="item in cardOptions" class="my-card q-pa-md q-ma-md" >
       <q-img
-        src="/icons/favicon-32x32.png"
-        style="height: 100px; max-width: 100px"
+        :src="item.image"
+        :style="item.style"
       />
       <q-card-section>
-        <div class="text-h5">Мировые новости, статистика и анализ</div>
+        <div class="text-h5">{{item.title}}</div>
         <div class="text-body1 text-black">
-          Краткое описание проекта.
+          {{item.description}}
         </div>
       </q-card-section>
 
       <q-card-actions>
+        <q-btn v-if="item.path.length" :to="item.path" flat label="Перейти" class="bg-green" />
         <q-space />
         <q-btn
-          color="grey"
+          color="black"
           round
           flat
           dense
-          :icon="expanded0 ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
-          @click="expanded0 = !expanded0"
+          :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
+          @click="expanded = !expanded"
         />
       </q-card-actions>
 
       <q-slide-transition>
-        <div v-show="expanded0">
+        <div v-show="expanded">
           <q-separator />
           <q-card-section class="text-subtitle2">
-            Проект создан с помощью javascript-фреймворка Quasar (Vue3).
-            Бэкенд представлят собой набор API, разработанный на PHP.
-            База данных заполняется скриптом, запускаемым на хостинге по расписанию
-            5 раз в сутки с 2019 года и на данный момент (январь 2025 г.) содержит около 4,5 млн. записей.
+            {{item.text}}
           </q-card-section>
         </div>
       </q-slide-transition>
     </q-card>
 
-
-    <q-card class="my-card q-pa-md q-ma-md" >
-      <q-img
-        src="/images/vuechart-example.png"
-      />
-      <q-card-section>
-        <div class="text-h5 ">СТАТИСТИКА</div>
-        <div class="text-body1  text-black">
-          В данном разделе можно увидеть частоту встречаемости слов в тексте новостей и заголовков.
-        </div>
-      </q-card-section>
-
-      <q-card-actions>
-        <q-btn :to="'/'" flat label="Перейти" class="bg-green" />
-
-        <q-space />
-
-        <q-btn
-          color="grey"
-          round
-          flat
-          dense
-          :icon="expanded1 ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
-          @click="expanded1 = !expanded1"
-        />
-      </q-card-actions>
-
-      <q-slide-transition>
-        <div v-show="expanded1">
-          <q-separator />
-          <q-card-section class="text-subtitle2">
-            {{ lorem }}
-          </q-card-section>
-        </div>
-      </q-slide-transition>
-    </q-card>
   </div>
 </template>
 
 <script setup>
 import {ref} from "vue";
 
-const expanded0 = ref(false)
-const expanded1 = ref(false)
-const lorem = ('Данный раздел позволяет задать слово, частоту встречаемости которого необходимо посчитать в новостях за определенный промежкток времени')
+const expanded = ref(false)
+
+const cardOptions = ref([
+  {
+    title:'Мировые новости, статистика и анализ',
+    description:'Краткое описание проекта',
+    image:'/icons/favicon-32x32.png',
+    style:'height: 100px; max-width: 100px',
+    path:'',
+    text:'Проект создан с помощью javascript-фреймворка Quasar (Vue3).\n' +
+      '            Бэкенд представлят собой набор API, разработанный на PHP.\n' +
+      '            База данных заполняется скриптом, запускаемым на хостинге по расписанию\n' +
+      '            5 раз в сутки с 2019 года и на данный момент (январь 2025 г.) содержит около 4,5 млн. записей.'
+  },
+  {
+    title:'СТАТИСТИКА',
+    description:' В данном разделе можно увидеть частоту встречаемости слов в тексте новостей и заголовков.',
+    image:'../src/assets/images/vuechart-example.png',
+    style:'',
+    path:'/',
+    text:'Данный раздел позволяет задать слово, частоту встречаемости которого необходимо посчитать в новостях за определенный промежкток времени'
+  },
+  {
+    title:'НОВОСТИ',
+    description:' Просмотр новостей с 2019 года по текущее время.',
+    image:'../src/assets/images/news.jpg',
+    style:'',
+    path:'/NewsPage/',
+    text:''
+  }
+])
+
 </script>
 
 <style lang="sass" scoped>
